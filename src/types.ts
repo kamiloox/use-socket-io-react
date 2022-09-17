65e38de9febfd2f8453e70549235abc2abc77216
Kamil Troczewski <kamil.troczewski@allegro.pl>
2022-09-17 20:22:22 +0200

    feat(useSocketEmit): better types suggestions

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import * as Client from 'socket.io-client';
+
 // eslint-disable-next-line functional/prefer-readonly-type
 export type UnknownArray = Array<unknown> | ReadonlyArray<unknown>;
 
@@ -9,8 +11,18 @@ export type MapToUndefined<Tuple> = {
   readonly [Key in keyof Tuple]: Tuple[Key] | undefined;
 };
 
+export type Socket = Client.Socket<ServerToClientEvents, ClientToServerEvents>;
+
 // Interfaces to augment down below ↓
 
-export interface ServerToClientEvents {}
+export interface ServerToClientEvents {
+  readonly [event: EventsOf<ServerToClientEvents>]: (
+    ...args: readonly any[]
+  ) => void;
+}
 
-export interface ClientToServerEvents {}
+export interface ClientToServerEvents {
+  readonly [event: EventsOf<ClientToServerEvents>]: (
+    ...args: readonly any[]
+  ) => void;
+}
